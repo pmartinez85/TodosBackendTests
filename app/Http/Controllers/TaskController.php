943d82c68d34815effc199ceb7ece98445597bae
Pedro Martinez <pmartinez1085@gmail.com>
2016-11-29 19:27:34 +0100

    sync

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+/**
+ * Class Task
+ * @package App\Http\Controllers
+ */
+class TaskController extends Controller
+{
+    public function index()
+    {
+
+    }
+}
---
+++ tests/TaskControllerTest.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+/**
+ * Class TaskControllerTest
+ */
+class TaskControllerTest extends TestCase
+{
+    /**
+     * A basic test example.
+     * @group todo
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+        //1 prepare
+        //2 Execute test
+        //3 comprovacions, assercions, shoulds, expectations
+    }
+
+}
+
